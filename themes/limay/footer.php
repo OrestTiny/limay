815c6609c8b6264772a08d3cdeb7f03900b50715
Orest Matsukh <orest.sd@gmail.com>
2024-02-04 21:17:28 +0200

    add

--- themes/limay/footer.php
+++ themes/limay/footer.php
@@ -20,36 +20,30 @@ $social = cmb2_get_option('limay_main_options', 'limay_social_group');
 
 <footer class="limay-footer">
 
-  <div class="limay-footer__wrap">
-    <div class="limay-footer__wrap-left">
+  <h4>FOLLOW</h4>
+  <ul class="limay-footer__social">
+    <?php if (!empty($social)) { ?>
+      <?php foreach ($social as $item) { ?>
+        <li>
+          <a href="<?php echo esc_url($item['limay_social_group_link']); ?>" title="<?php echo esc_html($item['limay_social_group_name']); ?>" aria-label="<?php echo esc_html($item['limay_social_group_name']); ?>" target="_blank" rel="noopener">
+            <?php echo file_get_contents(get_template_directory() . '/assets/images/icons/' . $item['limay_social_group_icon'] . '.svg'); ?>
+          </a>
+        </li>
+      <?php } ?>
+    <?php } ?>
+  </ul>
 
-      <div>
-        <h4>FOLLOW</h4>
-        <ul class="limay-footer__social">
-          <?php if (!empty($social)) { ?>
-            <?php foreach ($social as $item) { ?>
-              <li>
-                <a href="<?php echo esc_url($item['limay_social_group_link']); ?>" title="<?php echo esc_html($item['limay_social_group_name']); ?>" aria-label="<?php echo esc_html($item['limay_social_group_name']); ?>" target="_blank" rel="noopener">
-                  <?php echo file_get_contents(get_template_directory() . '/assets/images/icons/' . $item['limay_social_group_icon'] . '.svg'); ?>
-                </a>
-              </li>
-            <?php } ?>
-          <?php } ?>
-        </ul>
-      </div>
-    </div>
+  <?php if (has_nav_menu('footer-menu')) {
+    $args = array(
+      'container_class' => 'limay-footer__menu',
+      'container' => 'nav',
+      'menu_class' => 'footer-menu',
+      'theme_location' => 'footer-menu'
+    );
+    wp_nav_menu($args);
+  }  ?>
 
-    <?php if (has_nav_menu('footer-menu')) {
-      $args = array(
-        'container_class' => 'limay-footer__menu',
-        'container' => 'nav',
-        'menu_class' => 'footer-menu',
-        'theme_location' => 'footer-menu'
-      );
-      wp_nav_menu($args);
-    }  ?>
 
-  </div>
 </footer>
 
 </div>
